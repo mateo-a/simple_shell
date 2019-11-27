520099db8b6765a615c54976e652f657a43c0c12
sergioq7777 <55101797+Sergioq7777@users.noreply.github.com>
2019-11-27 14:27:40 -0500

    [FIX] Adding function

--- bigshell.c
+++ bigshell.c
@@ -1,6 +1,18 @@
 #include "bigshell.h"
 
-
+/**
+ * sig_handler - handle the interrupt signal
+ * @signo: int
+ * Return: void
+ */
+void sig_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, PROMPT, _strlen(PROMPT));
+	}
+}
 /**
  * main - Entry point of the program
  * @ac: Count arguments
