90b842efb988325b2d62d445fa7f03a928a13664
mateo-a <alexander.urrego@gmail.com>
2019-11-27 04:22:05 +0000

    [Feature] Check signal to ctrl-c

--- bigshell.c
+++ bigshell.c
@@ -1,22 +1,36 @@
 #include "bigshell.h"
 
 /**
- * main - Entry point of the program.
- * @ac: Count arguments.
+ * sig_handler - handle the interrupt signal
+ * @signo: int
+ * Return: void
+ */
+void sig_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, PROMPT, _strlen(PROMPT));
+	}
+}
+
+/**
+ * main - Entry point of the program
+ * @ac: Count arguments
  * @av: Arguments vector, name of the program
  * @env: Array of pointers to environment variables
  * Return: 0
  */
 int main(int ac, char *av[], char *env[])
 {
-	char *strn;
-	size_t size;
-	int count_commands;
 	(void)(ac);
 	(void)(env);
+	char *strn;
+	size_t size;
+	int count_commands = 0;
 
-	count_commands = 0;
-	signal(SIGINT, SIG_IGN);
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		printf("\ncan't catch SIGINT\n");
 	do {
 		count_commands++;
 		strn = NULL;
